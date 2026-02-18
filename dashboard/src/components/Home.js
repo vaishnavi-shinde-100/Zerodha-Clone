98cc54b7d23af96cc93aa7db1c019636bf41a4de
vaishnavi <shindevaishnavi992@gmail.com>
2026-02-18 22:39:15 +0530

    first commit

---
+++ dashboard/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import TopBar from "./TopBar";
+import Dashboard from "./Dashboard";
+
+const Home = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+};
+
+export default Home;
